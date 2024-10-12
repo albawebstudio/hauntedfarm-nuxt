068cb307b8abd4ab783623e43a2e0b87967149a9
Andrew Alba <andrew.alba@gmail.com>
2024-10-11 22:42:27 -0500

    🎨 style(Home.vue): improve readability by breaking down NuxtLink attributes into multiple lines ✨ feat(Home.vue): wrap FontAwesomeIcon in client-only tag to prevent server-side rendering and improve performance

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -27,12 +27,20 @@ const yrsHaunting = date.getFullYear() - site.value.established
         </NuxtLink>
       </div>
       <div class="flex mx-auto my-24 w-2/3">
-        <NuxtLink v-for="social_link in site.social_links" :key="social_link.label" :to="social_link.href" external class="mx-auto flex" target="_blank">
+        <NuxtLink
+            v-for="social_link in site.social_links"
+            :key="social_link.label"
+            :to="social_link.href"
+            external
+            class="mx-auto flex"
+            target="_blank">
           <div :class="social_link.bg_color" class="mx-auto flex h-16 w-16 -translate-y-12 transform items-center justify-center rounded-full shadow-lg">
-              <FontAwesomeIcon :icon="social_link.icon"
-                               :title="social_link.display_title"
-                               :class="social_link.color"
-                               class="text-2xl"/>
+              <client-only>
+                <FontAwesomeIcon :icon="social_link.icon"
+                                 :title="social_link.display_title"
+                                 :class="social_link.color"
+                                 class="text-2xl"/>
+              </client-only>
           </div>
         </NuxtLink>
       </div>
