5bbed10940e6f7d3e9487f8b9112937d51867a38
adnygaard <adnygaard_26@hotmail.com>
2025-07-11 12:58:47 -0500

    Update Coupon.vue
    changed amound saved from coupon from $2 to $3

--- app/components/common/Coupon.vue
+++ app/components/common/Coupon.vue
@@ -36,9 +36,9 @@ onMounted(() => {
 <template>
   <div class="bg-gradient-to-br from-slate-900 to-slate-700 text-white text-center py-5 sm:py-10 px-0 sm:px-20 rounded-lg shadow-md relative">
 
-<!--    <h3 class="text-2xl font-semibold mb-4">Save $2.00 off General Admission</h3>-->
+<!--    <h3 class="text-2xl font-semibold mb-4">Save $3.00 off General Admission</h3>-->
     <div class="flex justify-between items-center pb-4 px-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
-      <h3 class="text-lg font-semibold dark:text-white grow">Save $2.00 off General Admission</h3>
+      <h3 class="text-lg font-semibold dark:text-white grow">Save $3.00 off General Admission</h3>
       <button @click="cancelModal" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white flex-none" data-modal-toggle="updateProductModal">
         <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         <span class="sr-only">Close modal</span>
