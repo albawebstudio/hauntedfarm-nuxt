3b5fbda821099e286f67195500063fa9f1cac8a8
Alba Web Studio <support@albawebstudio.com>
2026-03-09 22:26:05 -0500

    feat(pages): add new index and legal article pages to enhance site content feat(plugins): introduce FontAwesome, reCAPTCHA, and Google Map plugins for improved functionality chore(styles): remove unused Tailwind CSS file to clean up the project structure

---
+++ app/app/pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "default",
+});
+</script>
+
+<template>
+  <div>
+    <Home/>
+
+    <About/>
+
+    <Tickets/>
+
+    <Schedule/>
+
+    <Media/>
+
+    <Contact/>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
